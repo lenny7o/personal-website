d77f14c7e703f55a9db0bf0d5a22b7fac97cb41a
lenny7__ <149390804+lenny7o@users.noreply.github.com>
2026-03-13 21:18:18 +0100

    feat(ui): add a button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,38 @@
+import { cn } from '@/lib/cn'
+
+type ButtonProps = {
+  children: React.ReactNode
+  variant?: keyof typeof variants
+  Icon?: React.FC<React.SVGProps<SVGSVGElement>>
+  className?: string
+}
+
+const base =
+  'px-5 py-3 text-base inline-flex items-center justify-center align-middle font-medium rounded-4xl cursor-pointer hover:scale-105 transition-transform group'
+
+const variants = {
+  primary: 'bg-primary text-text-inverse border border-primary',
+  secondary: 'bg-surface text-text border border-border',
+}
+
+export default function Button({
+  children,
+  variant = 'primary',
+  Icon,
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <button className={cn(base, variants[variant], className)} {...props}>
+      {children}
+      {Icon && (
+        <Icon
+          className="ml-2 -rotate-45 group-hover:rotate-0 transition-transform"
+          height={20}
+          width={20}
+          aria-hidden="true"
+        />
+      )}
+    </button>
+  )
+}
